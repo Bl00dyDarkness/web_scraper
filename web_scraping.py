b357d3da7ff4404e9511032bdc5e5c51f7837df1
YoniSR <20709828+Bl00dyDarkness@users.noreply.github.com>
2025-02-24 09:18:08 +0100

    missing dot in extension changed file name to img alt

--- web_scraping.py
+++ web_scraping.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 import requests
 import os
-from dotenv import load_dotenv
 
 # Load secret .env file
 load_dotenv("local.env")
@@ -12,7 +12,7 @@ local_folder = os.getenv('LOCAL_FOLDER')
 class_name = os.getenv('CLASS_NAME')
 
 #File extension, used jpg for commodity, dont know the best one, probably incorrect ext
-image_ext = "jpg"
+image_ext = ".jpg"
 
 #html request
 result = requests.get(url)
@@ -24,9 +24,15 @@ cards = doc.find_all("img", {"class":class_name})
 for card in cards:
     #get attr value
     imgURL = card['src']
-    #expecific possition at the url where its the name of the image
-    card_number = imgURL[66:69]
+    file_name = card['alt']
+    
     #get image
     img = requests.get(imgURL,allow_redirects=True)
     #save image
-    open(local_folder + card_number + image_ext,'wb').write(img.content)
+    img_name = local_folder + file_name + image_ext 
+
+    #Check if file exist
+    #if not os.path.isfile(img_name):
+
+    open(img_name,'wb').write(img.content)
+    print(img_name)
